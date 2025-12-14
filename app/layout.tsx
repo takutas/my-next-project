12a5dafa893d03471a0563142d66a79836db85ff
Takutas <takutas59@gmail.com>
2025-12-14 15:58:53 +0900

    4章まで完了

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="en">
       <body>
         <Header />
         {children}
